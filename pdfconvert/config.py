34dd14d3596f2648536fd121b0a4f2ce600d93ab
d.yavno <d.yavno@drweb.com>
2015-10-29 17:08:33 +0300

    added call to ghostscript to optimize pdf

--- pdfconvert/config.py
+++ pdfconvert/config.py
@@ -6,4 +6,5 @@ pdf2htmlex_directory = 'c:\\5\\Архив\\pdf2htmlex'
 dest_dir_specifier = '--dest-dir='
 converter_name = 'pdf2htmlex'
 additional_options = ['--fallback=1']
+ghostscript_path = 'c:\\Program Files (x86)\\gs\\gs9.18\\bin\\gswin32c.exe'
 
--- pdfconvert/script.py
+++ pdfconvert/script.py
@@ -8,12 +8,21 @@ import subprocess
 
 
 class PdfConverter:
-    def __init__(self, pdf_files_path, pdf_converter_path, dest_dir_specifier, converter_name, additional_options):
+    def __init__(self, pdf_files_path, pdf_converter_path, dest_dir_specifier, converter_name, additional_options, ghostscript_path):
         self.pdf_files_path = pdf_files_path
         self.pdf_converter_path = pdf_converter_path
         self.dest_dir_specifier = dest_dir_specifier
         self.converter_name = converter_name
         self.additional_options = additional_options
+        self.ghostscript_path = ghostscript_path
+        self.tmp_file_name = 'tmp.tmp'
+
+    def ghostcript_process(self, file_path):
+        tmp_file = os.path.join(os.path.dirname(file_path), self.tmp_file_name)
+        #os.system('"%s"' % self.ghostscript_path + '-sDEVICE=pdfwrite' + ' ' + '-sOutputFile="%s"' % tmp_file + '-dNOPAUSE' + '-dBATCH' + ' ' + file_path)
+        subprocess.call([self.ghostscript_path, '-sDEVICE=pdfwrite', '-sOutputFile=%s' % tmp_file, '-dNOPAUSE', '-dBATCH', file_path])
+        os.unlink(file_path)
+        os.rename(tmp_file, file_path)
 
     def convert(self):
         # command_line = os.path.join(self.pdf_converter_path, self.converter_name) + ' '
@@ -25,6 +34,8 @@ class PdfConverter:
             if not re.match('.+\.pdf', file_name):
                 continue
 
+            print("ghostscripting %s" % full_path)
+            self.ghostcript_process(full_path)
             args = [os.path.join(self.pdf_converter_path, self.converter_name)]
             for option in self.additional_options:
                 args.append(option)
@@ -40,7 +51,8 @@ def __main__():
                              config.pdf2htmlex_directory,
                              config.dest_dir_specifier,
                              config.converter_name,
-                             config.additional_options)
+                             config.additional_options,
+                             config.ghostscript_path)
     converter.convert()
 
 
