0f54898cb8392b740dba8a97db6631b20a9ec813
d.yavno <d.yavno@drweb.com>
2015-10-29 14:32:09 +0300

    added additional options

--- pdfconvert/config.py
+++ pdfconvert/config.py
@@ -5,4 +5,5 @@ pdf_files_directory = 'c:\\5\\Архив'
 pdf2htmlex_directory = 'c:\\5\\Архив\\pdf2htmlex'
 dest_dir_specifier = '--dest-dir='
 converter_name = 'pdf2htmlex'
+additional_options = ['--fallback=1']
 
--- pdfconvert/script.py
+++ pdfconvert/script.py
@@ -8,14 +8,15 @@ import subprocess
 
 
 class PdfConverter:
-    def __init__(self, pdf_files_path, pdf_converter_path, dest_dir_specifier, converter_name):
+    def __init__(self, pdf_files_path, pdf_converter_path, dest_dir_specifier, converter_name, additional_options):
         self.pdf_files_path = pdf_files_path
         self.pdf_converter_path = pdf_converter_path
         self.dest_dir_specifier = dest_dir_specifier
         self.converter_name = converter_name
+        self.additional_options = additional_options
 
     def convert(self):
-        #command_line = os.path.join(self.pdf_converter_path, self.converter_name) + ' '
+        # command_line = os.path.join(self.pdf_converter_path, self.converter_name) + ' '
         #command_line += self.dest_dir_specifier + self.pdf_files_path + ' '
         for file_name in os.listdir(self.pdf_files_path):
             full_path = os.path.join(self.pdf_files_path, file_name)
@@ -24,17 +25,22 @@ class PdfConverter:
             if not re.match('.+\.pdf', file_name):
                 continue
 
+            args = [os.path.join(self.pdf_converter_path, self.converter_name)]
+            for option in self.additional_options:
+                args.append(option)
+            args.append(self.dest_dir_specifier + self.pdf_files_path)
+            args.append(full_path)
+
             print("Converting %s" % full_path)
-            subprocess.call([os.path.join(self.pdf_converter_path, self.converter_name),
-                             self.dest_dir_specifier + self.pdf_files_path,
-                             full_path])
+            subprocess.call(args)
 
 
 def __main__():
     converter = PdfConverter(config.pdf_files_directory,
-                 config.pdf2htmlex_directory,
-                 config.dest_dir_specifier,
-                 config.converter_name)
+                             config.pdf2htmlex_directory,
+                             config.dest_dir_specifier,
+                             config.converter_name,
+                             config.additional_options)
     converter.convert()
 
 
